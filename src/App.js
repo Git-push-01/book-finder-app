cd83bdac4f508587bcf6b416e144e3f8a39996c1
Hugo Escandon <hescandon6@gmail.com>
2021-05-30 13:46:37 -0400

    axios for API requests

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-import './App.css';
+let API_URL = `https://www.googleapis.com/books/v1/volumes`;
 
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const onInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const fetchBooks = async () => {
+    // Ajax call to API using Axios
+    const result = await axios.get(`${API_URL}?q=${searchTerm}`);
+    // Books result
+    console.log(result.data);
+  };
+  const onSubmitHandler = (e) => {
+    // Prevent browser refreshing after form submission
+    e.preventDefault();
+    // Call fetch books async function
+    fetchBooks();
+  };
 
-function App() {
-  console.log(process.env.REACT_APP_BOOK_FINDER_API_KEY)
+  console.log(process.env.REACT_APP_BOOK_FINDER_API_KEY);
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <section>
+      <form onSubmit={onSubmitHandler}>
+        <label>
+          <span>Search for books</span>
+          <input
+            type="search"
+            placeholder="microservice, restful design, etc.,"
+            value={searchTerm}
+            onChange={onInputChange}
+          />
 
-
-        Hugo
-        good
-
-      </header>
-    </div>
+          <button type="submit">Search</button>
+        </label>
+      </form>
+    </section>
   );
-}
+};
 
 export default App;
